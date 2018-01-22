9ca0109cd82b7c7d3939c4f7c238bfea520e0166
hubo <hubo@ruitukeji.com>
2018-01-22 16:20:01 +0800

    '初始化'

---
+++ js/common.js
@@ -0,0 +1,52 @@
+$(function () {
+  // layui.define(['layer', 'code', 'form', 'element', 'util'], function (exports) {
+  //   var $ = layui.jquery, device = layui.device(), form = layui.form();
+  //   exports('index', {});//注意，这里是模块输出的核心，模块名必须和use时的模块名一致
+  // });
+  //
+  // layui.config({
+  //   base: '/res/js/modules/' //你的模块目录
+  // }).use('index'); //加载入口
+  
+  $(".has-sub").mouseover(function () {
+    $(this).find(".sub-nav").css({
+      "display": "block",
+      "height": "168px",
+      "opacity": "1",
+      "overflow": "inherit"
+    });
+  });
+  
+  $(".has-sub").mouseout(function () {
+    $(this).find(".sub-nav").css({
+      "height": "0",
+      "opacity": "0",
+      "overflow": "hidden"
+    });
+  })
+  
+  $(".has-subs").mouseover(function () {
+    $(this).find(".sub-navs").css({
+      "display": "block",
+      "height": "168px",
+      "opacity": "1",
+      "overflow": "hidden"
+    });
+  });
+  
+  $(".has-subs").mouseout(function () {
+    $(this).find(".sub-navs").css({
+      "height": "0",
+      "opacity": "0",
+      "overflow": "hidden"
+    });
+  })
+  
+  // go top
+  $('#topBtn').click(function () {
+    $('html,body').animate({
+      scrollTop: 0
+    }, 1000);
+  });
+  
+})
